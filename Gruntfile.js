54e49a11d11f1b32b983ec16c4ccc9ea7cd23e85
Anton Gorbikov <anton.gorbikov@gmail.com>
2014-09-18 23:01:31 +0300

    Add pre-commit hook. Setup Grunt.

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,11 +25,56 @@ module.exports = function(grunt) {
 				maxparams: 3,
 				maxdepth: 3
 			},
-			uses_defaults: ['src/*.js']
+			all: ['src/*.js']
+		},
+
+		copy: {
+			major: {
+				src: 'package.json',
+				dest: 'package.json',
+				options: {
+					process: function(content) {
+						return content.replace(/"version": "(\d+)\.(\d+)\.(\d+)"/, function(match, p1) {
+							return '"version": "' + (Number(p1) + 1) + '.0.0"';
+						});
+					}
+				}
+			},
+			minor: {
+				src: 'package.json',
+				dest: 'package.json',
+				options: {
+					process: function(content) {
+						return content.replace(/"version": "(\d+)\.(\d+)\.(\d+)"/, function(match, p1, p2) {
+							return '"version": "' + p1 + '.' + (Number(p2) + 1) + '.0"';
+						});
+					}
+				}
+			},
+			patch: {
+				src: 'package.json',
+				dest: 'package.json',
+				options: {
+					process: function(content) {
+						return content.replace(/"version": "(\d+)\.(\d+)\.(\d+)"/, function(match, p1, p2, p3) {
+							return '"version": "' + p1 + '.' + p2 + '.' + (Number(p3) + 1) + '"';
+						});
+					}
+				}
+			},
+			hooks: {
+				src: 'hooks/*',
+				dest: '.git/'
+			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 
+	grunt.registerTask('major', ['copy:major']);
+	grunt.registerTask('minor', ['copy:minor']);
+	grunt.registerTask('patch', ['copy:patch']);
+	grunt.registerTask('install', ['copy:hooks']);
 	grunt.registerTask('default', ['jshint']);
 };
